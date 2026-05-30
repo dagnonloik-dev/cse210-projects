6dde2edd9eb4e9cc216a3edf3001605b9feee8c2
Dagnon Loik <dagnonloik5@gmail.com>
2026-05-30 10:37:00 +0100

    Exercise3 added

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        while(response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1,101);
+            
+            Console.WriteLine($"What is the magic number? {randomNumber}");
+            int number = -1;
+            int counter = 0;
+
+            while(number != randomNumber)
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                number = int.Parse(answer);
+                counter++;
+
+                if(number > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if(number < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+            }
+                    
+            Console.WriteLine($"You guessed it in {counter} testing.");
+            Console.Write("Do you want to play again? (yes/no) ");
+            response = Console.ReadLine();
+        }
+        
+        
     }
 }
