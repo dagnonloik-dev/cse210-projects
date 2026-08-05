c2ead3a9a82cd659cf1f05efe200302fa359fa0f
Dagnon Loik <dagnonloik5@gmail.com>
2026-08-05 11:21:47 +0100

    Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment assignment2 = new MathAssignment("Samuel Bennett", "Fractions", "7.3", "8-19");
+        WritingAssignment assignment3 = new WritingAssignment("Samuel Bennett", "Composition", "My Family");
+        
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
