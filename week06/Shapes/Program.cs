4c004bf1bdc309254b148cae705350e71c98bd2a
Dagnon Loik <dagnonloik5@gmail.com>
2026-08-11 19:22:27 +0100

    shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes= new List<Shape>();
+
+        Square s1 = new Square(6,"Blue");
+        shapes.Add(s1);
+
+        Circle s2 = new Circle(6,"Red");
+        shapes.Add(s2);
+
+        Rectangle s3 = new Rectangle(6,4,"Green");
+        shapes.Add(s3);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");   
+        }
     }
 }
